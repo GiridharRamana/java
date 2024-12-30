cda1831b94b916bac3c2b2e5ca3464d3c8f557c2
Ramana <giridharramana.morla0@gmail.com>
2024-12-30 13:10:01 +0530

    1st commit

---
+++ ramana/src/main/java/com/giridhar/ramana/RamanaApplication.java
@@ -0,0 +1,13 @@
+package com.giridhar.ramana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RamanaApplication.class, args);
+	}
+
+}
